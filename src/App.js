077ef6cf4a6270d109d4353bb9af9151bb52ea23
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-09-17 23:37:10 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import GetStarted from './commonScreens/getStarted/GetStarted';
-import AccountSignUp from './commonScreens/signUp/AccountSignUp';
-import AccountSignIn from './commonScreens/signIn/AccountSignIn';
-import Home from './adminScreens/adminHome/Home';
-import { Route, Routes } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import GetStarted from "./commonScreens/getStarted/GetStarted";
+import AccountSignUp from "./commonScreens/signUp/AccountSignUp";
+import AccountSignIn from "./commonScreens/signIn/AccountSignIn";
+import Home from "./adminScreens/adminHome/Home";
+import AddItem from "./adminScreens/adminAddItem/AddItem";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -14,8 +15,7 @@ function App() {
         <Route path="/signUp" element={<AccountSignUp />} />
         <Route path="/signIn" element={<AccountSignIn />} />
         <Route path="/adminHome" element={<Home />} />
-
-
+        <Route path="/adminAddItem" element={<AddItem />} />
       </Routes>
     </div>
   );
